83be497d1f8f473f3d6649eb921d01287b6c5e86
JuanSolo5 <juanscazon@gmail.com>
2024-08-29 17:54:52 -0300

    Update TP2.py

---
+++ TP2.py
@@ -0,0 +1,9 @@
+class usuario:
+    def __init__(self, nombre, contr, email, dir, tel, estado):
+        self.nombre = nombre
+        self.contr = contr
+        self.email = email
+        self.dir = dir
+        self.tel = tel
+        self.estado = estado
+
