a738fa1cc202f693e9d00cd831db904d28ef41c1
JuanSolo5 <juanscazon@gmail.com>
2024-09-04 12:33:45 -0300

    Update TP2.py
    menu creado

--- TP2.py
+++ TP2.py
@@ -7,5 +7,29 @@ class usuario:
         self.tel = tel
         self.estado = estado
 
-
 ## Hola Juancito
+
+listaUser=[]
+
+
+
+def menuUsuarios(listaUser):
+    menuActivo = True
+    while menuActivo:
+        print("MENU:")
+        print("1) consultar usuarios")
+        print("2) agregar usuario")
+        print("3) modificar usuario")
+        print("4) eliminar usuario")
+
+        opcion = input("Ingrese una opcion: ")
+
+        match opcion:
+            case "1":
+                consultarUsuarios(listaUser)
+            case "2":
+                agregarUsuario(listaUser)
+            case "3":
+                modificarUsuario(listaUser)
+            case "4":
+                eliminarUsuario(listaUser)
