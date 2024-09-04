e84bc05c89eb9ebed471c8eb7cbd2e503de4dde8
JuanSolo5 <juanscazon@gmail.com>
2024-09-04 14:31:14 -0300

    Update TP2.py
    agregado las sucursales mas funcionalidades

--- TP2.py
+++ TP2.py
@@ -95,4 +95,105 @@ def menuUsuarios(listaUser):
             case "5":
                 menuActivo = False
 
-menuUsuarios(listaUsuarios)
+#menuUsuarios(listaUsuarios)
+
+# login de usuarios
+def loginUsuario(listaUser):
+    nombreE = input("Ingrese el nombre ")
+    contrE = input("ingrese la contraseña ")
+    usuario_encontrado = False
+    for i in range(len(listaUser)):
+        if listaUser[i].nombre == nombreE and listaUser[i].contr == contrE:
+            usuario_encontrado = True
+            usuarioLogueado = listaUser[i]
+
+    if usuario_encontrado == False:
+        print("Datos incorrectos o inexistentes")
+    else:
+        print("Acceso concedido")
+
+    return usuario_encontrado
+
+# clase de sucursales
+class Sucursal:
+    def __init__(self, nombre, direccion):
+        self.nombre = nombre
+        self.direccion = direccion
+
+    def __str__(self):
+        return f"Nombre del comercio: {self.nombre}, Dirección: {self.direccion}"
+    
+
+# Lista para almacenar las sucursales
+listaSucursales = []
+
+# Consulta la lista de todas las sucursales
+def consultarSucursal(listaSucursales):
+    for i in listaSucursales:
+        print(i)
+
+# Agrega una nueva sucursal a la lista
+def agregarSucursal(listaSucursales):
+    nombre = input("Ingrese el nombre del comercio: ")
+    direccion = input("Ingrese la dirección: ")
+    nuevaSucursal = Sucursal(nombre, direccion)
+    listaSucursales.append(nuevaSucursal)
+    print(f"Sucursal {nuevaSucursal} agregada a la lista.")
+
+# Modifica una sucursal existente
+def modificarSucursal(listaSucursales):
+    i = int(input("Ingrese el número de la sucursal a modificar: "))
+    if i < 0 or i >= len(listaSucursales):
+        print("Índice fuera de rango.")
+    else:
+        sucursal_selec = listaSucursales[i]
+           
+        print("Deje el campo vacío si no desea modificar ese atributo.")
+        nombre = input(f"Ingrese nuevo nombre del comercio ({sucursal_selec.nombre}): ") or sucursal_selec.nombre
+        direccion = input(f"Ingrese nueva dirección ({sucursal_selec.direccion}): ") or sucursal_selec.direccion
+
+        sucursal_selec.nombre = nombre
+        sucursal_selec.direccion = direccion
+
+        print(f"Sucursal {sucursal_selec} modificada")
+
+# elimina una sucursal
+def eliminarSucursal(listaSucursales):
+    nombreS = input("Ingrese el nombre del comercio: ")
+    direccionS = input("Ingrese la dirección: ")
+    sucursal_encontrada = False
+
+    for i in range(len(listaSucursales)):
+        if listaSucursales[i].nombre == nombreS and listaSucursales[i].direccion == direccionS:
+            sucursal_encontrada = True
+            sucursal_eliminada = listaSucursales.pop(i)
+            print(f"La sucursal {sucursal_eliminada} fue eliminada")
+
+    if not sucursal_encontrada:
+        print("La sucursal no fue encontrada")
+
+# Ejecuta las funciones en un menú para las sucursales
+def menuSucursales(listaSucursales):
+    menuActivo = True
+    while menuActivo:
+        print("MENU:")
+        print("1) Consultar sucursales")
+        print("2) Agregar sucursal")
+        print("3) Modificar sucursal")
+        print("4) Eliminar sucursal")
+        print("5) Terminar programa")
+
+        opcion = input("Ingrese una opción: ")
+
+        match opcion:
+            case "1":
+                consultarSucursal(listaSucursales)
+            case "2":
+                agregarSucursal(listaSucursales)
+            case "3":
+                modificarSucursal(listaSucursales)
+            case "4":
+                eliminarSucursal(listaSucursales)
+            case "5":
+                menuActivo = False
+
