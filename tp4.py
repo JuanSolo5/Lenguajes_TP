fed4f0ef3808910315d01e5fc671c0eb2a922e27
Brian Frank <brianfrank93@gmail.com>
2024-09-21 18:01:30 -0300

    condiciones para roles

--- tp4.py
+++ tp4.py
@@ -40,17 +40,57 @@ class Database:
         conn.close()
         return user
 
+    @staticmethod
+    def get_user_role(username):
+        conn = Database.connect()
+        cursor = conn.cursor()
+        query = "SELECT rol FROM users WHERE name = %s"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if result:
+            return result[0]
+        else:
+            return None
+
 
 ## Manejo de usuario para tp 4
 def login_user_ui():
     username = input("Enter username: ")
     password = getpass.getpass("Enter password: ")
     user = Database.login(username, password)
+    user = Database.login(username, password)
     if user:
         print("Login successful!")
+        role = Database.get_user_role(username)
 
+        if role:
+            print(f"The role of {username} is {role}.")
+            if role == "Administrador":
+                admin_actions()
+            elif role == "Usuario":
+                user_actions()
+            else:
+                print("Unknown role.")
+        else:
+            print("Role not found.")
     else:
         print("Invalid username or password.")
+
+
+def admin_actions():
+    print("Performing admin actions...")
+
+    print("Admin action 1")
+    print("Admin action 2")
+
+
+def user_actions():
+    print("Performing user actions...")
+
+    print("User action 1")
+    print("User action 2")
 
 
 login_user_ui()
