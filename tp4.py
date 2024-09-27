98d6bc7f08ccebecd6a17fac29788f91988e58c3
Santiago383 <81712490+Santiago383@users.noreply.github.com>
2024-09-27 13:04:27 -0300

    updates para primer login/random password

--- tp4.py
+++ tp4.py
@@ -1,11 +1,13 @@
 import mysql.connector
 import getpass  ## para que no sea visible la pass en consola al escribirla
 import sys  ## para poder cerrar la terminal de PY
+import random
+import string
+
 from lenTP2db import (
-    create_user_ui,
-    read_users_ui,
-    update_user_ui,
-    delete_user_ui,
+    main_menu,
+    read_users_ui_unique,
+    update_password_ui,
 )  ## me traigo las funciones que necesito de " lenTP2db.py "
 
 config = {
@@ -37,6 +39,18 @@ class Database:
         conn.close()
 
     @staticmethod
+    def update(table, any_id, **kwargs):
+        conn = Database.connect()
+        cursor = conn.cursor()
+        columns = " = %s, ".join(kwargs.keys()) + " = %s"
+        values = tuple(kwargs.values())
+        query = f"UPDATE {table} SET {columns} WHERE id = %s"
+        cursor.execute(query, values + (any_id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    @staticmethod
     def login(username, password):
         conn = Database.connect()
         cursor = conn.cursor()
@@ -48,11 +62,11 @@ class Database:
         return user
 
     @staticmethod
-    def get_user_role(username):
+    def get_user_role(user_id):
         conn = Database.connect()
         cursor = conn.cursor()
-        query = "SELECT rol FROM users WHERE name = %s"
-        cursor.execute(query, (username,))
+        query = "SELECT rol FROM users WHERE id = %s"
+        cursor.execute(query, (user_id,))
         result = cursor.fetchone()
         cursor.close()
         conn.close()
@@ -61,79 +75,93 @@ class Database:
         else:
             return None
 
+    @staticmethod
+    def read_by_email(email):
+        conn = Database.connect()
+        cursor = conn.cursor()
+        query = f"SELECT * FROM users WHERE email = %s"
+        cursor.execute(query, (email,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return result
+
 
 ## Manejo de usuario para tp 4
 def login_user_ui():
-    username = input("Enter username: ")
-    password = getpass.getpass("Enter password: ")
-    user = Database.login(username, password)
-    user = Database.login(username, password)
+    email = input("Enter email: ")  # pedimos el email porque el username podria estar repetido
+    user = Database.read_by_email(email)
     if user:
-        print("Login successful!")
-        role = Database.get_user_role(username)
+        if user[9]: # la columna 10 es el atributo 'first_login'
+            new_password = generate_password()
+            print(f"Your password is: {new_password}")
+            Database.update("users", user[0], password=new_password, first_login=False)
+            return
 
-        if role:
-            print(f"The role of {username} is {role}.")
-            if role == "Administrador":
-                admin_actions()
-            elif role == "Empleado":
-                user_actions()
+        password = input("Enter password: ")  #si no pedimos el pw normalmente
+        if user[3] == password:
+            print("Login successful.")
+            role = Database.get_user_role(user[0])
+            if role:
+                if role == "Administrador":
+                    print("You have administrator credentials")
+                    admin_actions()
+                elif role == "Empleado":
+                    user_actions(user)
+                else:
+                    print("Unknown role.")
             else:
-                print("Unknown role.")
+                print("Role not found.")
         else:
-            print("Role not found.")
-    else:
-        print("Invalid username or password.")
+            print("Invalid credentials.")
 
 
 ## ROL ADMIN
 def admin_actions():
-    user_management_admin()
-
-
-def user_management_admin():
-    print("\n--- User Administrador ---")
-    print("1. Create User")
-    print("2. Update User")
-    print("3. Delete User")
-    print("4. Exit")
-
-    choice = input("Select an option (1-3): ")
-
-    if choice == "1":
-        create_user_ui()
-    elif choice == "2":
-        update_user_ui()
-    elif choice == "3":
-        delete_user_ui()
-    elif choice == "4":
-        print("Closing terminal...")
-        sys.exit()
-    else:
-        print("Invalid choice, please try again.")
-        user_management_admin()
+    main_menu() # llamamos al main menu del otro archivo que tiene todas las operaciones CRUD que pide el tp
 
 
 ## ROL EMPLEADO
-def user_actions():
-    user_management_employee()
+def user_actions(user):
+    user_management_employee(user)
 
 
-def user_management_employee():
+def user_management_employee(user):
     print("\n--- User Employee ---")
-    print("1. Read Users")
-    print("2. Exit")
+    print("1. Read User")
+    print("2. Change Password")
+    print("3. Exit")
 
     choice = input("Select an option (1-3): ")
 
     if choice == "1":
-        read_users_ui()
-    elif choice == "2":
+        read_users_ui_unique(user)
+        user_management_employee(user)
+    elif choice == "3":
         print("Closing terminal...")
         sys.exit()
+
+    elif choice == "2":
+        update_password_ui(user[0])
+        user_management_employee(user)
     else:
         print("Invalid choice, please try again.")
-        user_management_employee()
+        user_management_employee(user)
+
+
+def generate_password():
+    password_chars = [  # para que tenga al menos una letra y un digito
+        random.choice(string.ascii_letters),
+        random.choice(string.digits)
+    ]
+
+    characters = string.ascii_letters + string.digits
+    password_chars += random.choices(characters, k=6)   # agregamos otros 6 caracteres random
+
+    random.shuffle(password_chars)
+
+    password = ''.join(password_chars)
+    return password
 
 
 ## Es para que se ejecute solo este codigo, ya que importe las funciones del otro archivo
