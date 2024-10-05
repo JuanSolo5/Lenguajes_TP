a238bdb6c18c6405328c5b13bd8e5e00d87ffa96
JuanSolo5 <118191769+JuanSolo5@users.noreply.github.com>
2024-10-05 19:52:29 -0300

    Renombre crud_example.sql a tp4_bd.sql

--- tp4_bd.sql
+++ tp4_bd.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `crud_example`
+-- Base de datos: `tp4_bd`
 --
 
 -- --------------------------------------------------------
