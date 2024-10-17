967c7f1abfd7e8e343118a808776098e20638451
JuanSolo5 <juanscazon@gmail.com>
2024-10-17 12:21:50 -0300

    Modificado a patron MVC
    Renombrado de archivos a controller y model.
    +model.py se encarga de la comunicacion con la base de datos.
    +controller.py se encarga de la interfaz logica con el usuario.

--- controller.py
+++ controller.py
@@ -1,101 +1,19 @@
-import mysql.connector
-import getpass  # para que no sea visible la pass en consola al escribirla
+from getpass import getpass  # para que no sea visible la pass en consola al escribirla
 import sys
 import random
 import string
 
+from model import Database
 #color de los logs (instalar colorama en la terminal)
 #pip install colorama
 from colorama import Fore, Style, init
 # Inicializar colorama
 init(autoreset=True)
 
-config = {
-    "user": "root",
-    "password": "",
-    "host": "localhost",
-    "database": "tp4_bd",
-}
 
 
-class Database:
-    def __init__(self, name):
-        self.name = name
-
-    @staticmethod
-    def connect():
-        return mysql.connector.connect(**config)
-
-    @staticmethod
-    def create(table, **kwargs):
-        conn = Database.connect()
-        cursor = conn.cursor()
-        columns = ", ".join(kwargs.keys())
-        values = tuple(kwargs.values())
-        query = f"INSERT INTO {table} ({columns}) VALUES ({', '.join(['%s'] * len(values))})"
-        cursor.execute(query, values)
-        conn.commit()
-        cursor.close()
-        conn.close()
-
-    @staticmethod
-    def read(table):
-        conn = Database.connect()
-        cursor = conn.cursor()
-        query = f"SELECT * FROM {table}"
-        cursor.execute(query)
-        result = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return result
-
-    @staticmethod
-    def read_by_id(table, any_id):
-        conn = Database.connect()
-        cursor = conn.cursor()
-        query = f"SELECT * FROM {table} WHERE id = %s"
-        cursor.execute(query, (any_id,))
-        result = cursor.fetchone()
-        cursor.close()
-        conn.close()
-        return result
-
-    @staticmethod
-    def read_by_name(table, name):
-        conn = Database.connect()
-        cursor = conn.cursor()
-        query = f"SELECT * FROM {table} WHERE name = %s"
-        cursor.execute(query, (name,))
-        result = cursor.fetchone()
-        cursor.close()
-        conn.close()
-        return result
-
-    @staticmethod
-    def update(table, any_id, **kwargs):
-        conn = Database.connect()
-        cursor = conn.cursor()
-        columns = " = %s, ".join(kwargs.keys()) + " = %s"
-        values = tuple(kwargs.values())
-        query = f"UPDATE {table} SET {columns} WHERE id = %s"
-        cursor.execute(query, values + (any_id,))
-        conn.commit()
-        cursor.close()
-        conn.close()
-
-    @staticmethod
-    def delete(table, any_id):
-        conn = Database.connect()
-        cursor = conn.cursor()
-        query = f"DELETE FROM {table} WHERE id = %s"
-        cursor.execute(query, (any_id,))
-        conn.commit()
-        cursor.close()
-        conn.close()
-
-
+# Función principal del menú
 def main_menu():
-
     print("\n--- Administrator Main Menu ---")
     print("1. User Management")
     print("2. Store Management")
@@ -112,7 +30,6 @@ def main_menu():
     else:
         print("Invalid choice, please try again.")
         main_menu()
-
 
 def user_management():
     print("\n--- User Management ---")
@@ -199,34 +116,26 @@ def create_user_ui():
     name = input("Enter name: ")
     email = input("Enter email: ")
     password = generate_password()
-    rol = select_role()
+    role = select_role()
     telephone = input("Enter telephone: ")
     address = input("Enter address: ")
     status = 1  # inicializa en activo
 
-    # Verificar si el store_id existe en la base de datos
     store_id = input("Enter store ID: ")
     store = Database.read_by_id("stores", store_id)
-    
     if not store:
-        print(f"{Fore.RED}Error: The store with ID {store_id} does not exist.{Fore.RESET}")
-        user_management()  # Vuelve al menú de administración de usuarios
+        print(f"Error: The store with ID {store_id} does not exist.")
+        user_management()
         return
 
-    # Si la tienda existe, procede a crear el usuario
-    Database.create(
-        "users",
-        name=name,
-        email=email,
-        password=password,
-        rol=rol,
-        telephone=telephone,
-        address=address,
-        status=status,
-        store_id=store_id,
-    )
-    
+    Database.create("users", name=name, email=email, password=password, rol=role, telephone=telephone, address=address, status=status, store_id=store_id)
     print("User created successfully.")
+    user_management()
+
+def read_users_ui():
+    users = Database.read("users")
+    for user in users:
+        log_print_user(user)
     user_management()
 
 
@@ -444,6 +353,72 @@ def generate_password():
     return password
 
 
+## Manejo de usuario
+def login_user_ui():
+    print("--- Login ---")
+    email = input("Enter email: ")
+    user = Database.read_by_email(email)
+
+    if user:
+        if user[7] == 2:  # columna status == blocked
+            print("Blocked account.")
+            sys.exit()
+
+        password = getpass("Enter password: ")
+        if user[3] == password:
+            print("Login successful.")
+            role = Database.get_user_role(user[0])
+            if role:
+                if role == "Administrador":
+                    print("You have administrator credentials")
+                    admin_actions()
+                elif role == "Empleado":
+                    user_actions(user)
+                else:
+                    print("Unknown role.")
+            else:
+                print("Role not found.")
+        else:
+            print("Invalid credentials.")
+            sys.exit()
+    else:
+        print("User not found.")
+        sys.exit()
+
+## ROL ADMIN
+def admin_actions():
+    main_menu()  # llamamos al main menu del otro archivo que tiene todas las operaciones CRUD que pide el tp
+
+
+## ROL EMPLEADO
+def user_actions(user):
+    user_management_employee(user)
+
+def user_management_employee(user):
+    print("\n--- User Employee ---")
+    print("1. Personal Info")
+    print("2. Change Password")
+    print("3. Exit")
+
+    choice = input("Select an option (1-3): ")
+
+    if choice == "1":
+        read_users_ui_unique(user)
+        user_management_employee(user)
+    elif choice == "3":
+        print("Closing terminal...")
+        sys.exit()
+
+    elif choice == "2":
+        update_password(user[0])
+        user_management_employee(user)
+    else:
+        print("Invalid choice, please try again.")
+        user_management_employee(user)
+
+
+
+
 ## Es para que se ejecute solo este codigo, ya que importe las funciones del otro archivo
 if __name__ == "__main__":
-    main_menu()
+    login_user_ui()
--- model.py
+++ model.py
@@ -1,12 +1,4 @@
 import mysql.connector
-from getpass import getpass  ## para que no sea visible la pass en consola al escribirla
-import sys  ## para poder cerrar la terminal de PY
-
-from tp4functions import (
-    main_menu,
-    read_users_ui_unique,
-    update_password
-)  ## me traigo las funciones que necesito de " tp4functions.py "
 
 config = {
     "user": "root",
@@ -15,11 +7,7 @@ config = {
     "database": "tp4_bd",
 }
 
-
 class Database:
-    def __init__(self, name):
-        self.name = name
-
     @staticmethod
     def connect():
         return mysql.connector.connect(**config)
@@ -37,6 +25,28 @@ class Database:
         conn.close()
 
     @staticmethod
+    def read(table):
+        conn = Database.connect()
+        cursor = conn.cursor()
+        query = f"SELECT * FROM {table}"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def read_by_id(table, any_id):
+        conn = Database.connect()
+        cursor = conn.cursor()
+        query = f"SELECT * FROM {table} WHERE id = %s"
+        cursor.execute(query, (any_id,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
     def update(table, any_id, **kwargs):
         conn = Database.connect()
         cursor = conn.cursor()
@@ -44,6 +54,16 @@ class Database:
         values = tuple(kwargs.values())
         query = f"UPDATE {table} SET {columns} WHERE id = %s"
         cursor.execute(query, values + (any_id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    @staticmethod
+    def delete(table, any_id):
+        conn = Database.connect()
+        cursor = conn.cursor()
+        query = f"DELETE FROM {table} WHERE id = %s"
+        cursor.execute(query, (any_id,))
         conn.commit()
         cursor.close()
         conn.close()
@@ -57,90 +77,15 @@ class Database:
         result = cursor.fetchone()
         cursor.close()
         conn.close()
-        if result:
-            return result[0]
-        else:
-            return None
+        return result[0] if result else None
 
     @staticmethod
     def read_by_email(email):
         conn = Database.connect()
         cursor = conn.cursor()
-        query = f"SELECT * FROM users WHERE email = %s"
+        query = "SELECT * FROM users WHERE email = %s"
         cursor.execute(query, (email,))
         result = cursor.fetchone()
         cursor.close()
         conn.close()
         return result
-
-
-## Manejo de usuario
-def login_user_ui():
-    print("--- Login ---")
-    email = input("Enter email: ")
-    user = Database.read_by_email(email)
-
-    if user:
-        if user[7] == 2:  # columna status == blocked
-            print("Blocked account.")
-            sys.exit()
-
-        password = getpass("Enter password: ")
-        if user[3] == password:
-            print("Login successful.")
-            role = Database.get_user_role(user[0])
-            if role:
-                if role == "Administrador":
-                    print("You have administrator credentials")
-                    admin_actions()
-                elif role == "Empleado":
-                    user_actions(user)
-                else:
-                    print("Unknown role.")
-            else:
-                print("Role not found.")
-        else:
-            print("Invalid credentials.")
-            sys.exit()
-    else:
-        print("User not found.")
-        sys.exit()
-
-
-## ROL ADMIN
-def admin_actions():
-    main_menu()  # llamamos al main menu del otro archivo que tiene todas las operaciones CRUD que pide el tp
-
-
-## ROL EMPLEADO
-def user_actions(user):
-    user_management_employee(user)
-
-
-def user_management_employee(user):
-    print("\n--- User Employee ---")
-    print("1. Personal Info")
-    print("2. Change Password")
-    print("3. Exit")
-
-    choice = input("Select an option (1-3): ")
-
-    if choice == "1":
-        read_users_ui_unique(user)
-        user_management_employee(user)
-    elif choice == "3":
-        print("Closing terminal...")
-        sys.exit()
-
-    elif choice == "2":
-        update_password(user[0])
-        user_management_employee(user)
-    else:
-        print("Invalid choice, please try again.")
-        user_management_employee(user)
-
-
-# Es para que se ejecute solo este codigo, ya que importe las funciones del otro archivo
-if __name__ == "__main__":
-
-    login_user_ui()
